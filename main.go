859d45f430a3f5f6373531a7725e9f12ddbb0174
ABHAY <abhaybalakrishnan884@gmail.com>
2024-11-02 23:08:25 +0530

    added some funcs

--- main.go
+++ main.go
@@ -9,15 +9,48 @@ import (
 )
 
 type Task struct {
-    task string;
-    completed bool;
+	task      string
+	completed bool
+}
+
+func showMenu() {
+	fmt.Println("\nMenu:")
+	fmt.Println("1. Show Tasks")
+	fmt.Println("2. Add Task")
+	fmt.Println("3. Mark Task as Completed")
+	fmt.Println("4. Save Tasks to File")
+	fmt.Println("5. Exit")
+}
+
+func getUserInput(prompt string) string {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print(prompt)
+    input, _ := reader.ReadString('\n')
+    return strings.TrimSpace(input)
 }
 
 func main() {
-    tasks :=[]Task{}
+	tasks := []Task{}
 
-    for {
-        showMenu()
-        option := getUserInput("Enter your choice")
-    }
+	for {
+		showMenu()
+
+		option := getUserInput("Enter your choice")
+
+		switch option {
+            case "1": 
+                showTasks(tasks)
+            case "2": 
+                addTask(&tasks)
+            case "3": 
+                markTaskCompleted(&tasks)
+            case "4": 
+                saveTasksToFile(tasks)
+            case "5": 
+                fmt.Println("Exiting the ToDo application.")
+                return
+            default:
+                fmt.Println("Invalid choice. Please try again")
+		}
+	}
 }
