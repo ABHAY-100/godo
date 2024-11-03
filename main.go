4620a3344dbed1527f4b30b9b3297ba5148cbea4
ABHAY <abhaybalakrishnan884@gmail.com>
2024-11-03 15:00:30 +0530

    completed

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Task struct {
-	task      string
+	text string
 	completed bool
 }
 
 func showMenu() {
-	fmt.Println("\nMenu:")
+	time.Sleep(1 * time.Second)
+	fmt.Println("\n\nMenu:")
 	fmt.Println("1. Show Tasks")
 	fmt.Println("2. Add Task")
 	fmt.Println("3. Mark Task as Completed")
@@ -29,13 +31,63 @@ func getUserInput(prompt string) string {
     return strings.TrimSpace(input)
 }
 
+func showTasks(tasks []Task) {
+	if len(tasks) == 0 {
+		fmt.Println("**No tasks available**")
+		return
+	}
+	fmt.Println("Tasks:")
+	for i, task := range tasks {
+		status := " "
+		if task.completed {
+			status = "x"
+		}
+		fmt.Printf("%d. [%s] %s\n", i+1, status, task.text)
+	}
+}
+
+func addTask(tasks *[]Task) {
+	taskText := getUserInput("Enter task description: ")
+	*tasks = append(*tasks, Task{text: taskText})
+	fmt.Println("Task added.")
+}
+
+func markTaskCompleted(tasks *[]Task) {
+	showTasks(*tasks)
+	taskIndexStr := getUserInput("\nEnter task number to mark as completed: ")
+	taskIndex, err := strconv.Atoi(taskIndexStr)
+	if err != nil || taskIndex < 1 || taskIndex > len(*tasks) {
+	 fmt.Println("Invalid task number. Please try again.")
+	 return
+	}
+	(*tasks)[taskIndex-1].completed = true
+	fmt.Println("Task marked as completed.")
+}
+
+func saveTasksToFile(tasks []Task) {
+	file, err := os.Create("tasks.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+	for _, task := range tasks {
+		status := " "
+		if task.completed {
+			status = "x"
+		}
+		file.WriteString(fmt.Sprintf("[%s] %s\n", status, task.text))
+	}
+	fmt.Println("Tasks saved to file 'tasks.txt'.")
+}
+
 func main() {
 	tasks := []Task{}
 
 	for {
 		showMenu()
 
-		option := getUserInput("Enter your choice")
+		option := getUserInput("\nEnter your choice: ")
 
 		switch option {
             case "1": 
